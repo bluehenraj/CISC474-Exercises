5295605b915c4ed3c474b7aa2b8e41c5c880b766
Raj Trivedi <triudrav@udel.edu>
2021-12-08 16:41:37 -0500

    All the APIs are implemented now and are working fine

--- node-server/app.js
+++ node-server/app.js
@@ -3,47 +3,60 @@ const fs = require("fs");
 const path = require('path');
 const app = express();
 const port = 8080;
-let userNum = 0;
-
-function trackUserID(){
-    userNum = 0;
-    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
-        data = JSON.parse(data);
-        for(let user in data){
-            userNum++;
-        }
-    }); 
-    userNum++;
-}
-
-trackUserID();
 
 app.use( function ( req, res, next ) {
     const { url, path: routePath } = req ;
     console.log( 'Request: Timestamp:', new Date().toLocaleString(), ', URL (' + url + '), PATH (' + routePath + ').' ) ;
     next();
 });
+
 app.use('/', express.static(path.join(__dirname, '')))
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`)
 });
 
 app.get('/api/v1/listUsers', function(req, res) {
     fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
-    trackUserID();
-    console.log (data);
-    res.end(data);
+        console.log (data);
+        res.end(data);
+    });
+});
+
+app.get('/api/v1/:userid', function(req, res) {
+    // console.log(req.params.userid);
+    // console.log(typeof req.params.userid); --> String
+
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
+        data = JSON.parse(data);
+        
+        // console.log(data["user" + req.params.userid]);
+
+        let user = "user" + req.params.userid;
+
+        // Check if the user with the given userid exists or not
+        // If it doesn't exist, don't do anything
+        if(data[user]){
+            let filteredUser = {
+                [user] : {
+                    "name": data[user]["name"],
+                    "password": data[user]["password"],
+                    "profession": data[user]["profession"],
+                    "id": data[user]["id"]
+                }
+            }
+    
+            console.log(filteredUser);
+            res.end(JSON.stringify(filteredUser));
+        }
     });
 });
 
 app.post('/api/v1/addUser' , (req , res)=>{
-
     fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
-
         data = JSON.parse(data);
 
-        currentUser = "user" + userNum; // Keeps track of user ID to use for naming JSON object
-        userNum++;
+        currentUser = "user" + req.query["user"];
 
         let newUser = {
             [currentUser] : {
@@ -62,33 +75,34 @@ app.post('/api/v1/addUser' , (req , res)=>{
 
         fs.writeFile(__dirname + "/data/users.json", JSON.stringify(data), err => {
          if (err) {
-         console.error(err);
-         return;
+            console.error(err);
+            return;
          }
         });
+
         console.log(data);
         res.end(JSON.stringify(data));
     });
 });
 
 app.delete('/api/v1/deleteUser', function(req, res){
-
-    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
-     
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){  
         // console.log(data); // Data of users.json file in JSON Format
      
-     data = JSON.parse(data);
+        data = JSON.parse(data);
      
-     // console.log(data); // Data of users.json file as Javascript Object
+        // console.log(data); // Data of users.json file as Javascript Object
 
-     delete data["user"+req.query["user"]];
-     fs.writeFile(__dirname + "/data/users.json", JSON.stringify(data), err => {
-      if (err) {
-      console.error(err);
-      return;
-      }
-     });
-     console.log(data);
-     res.end(JSON.stringify(data));
+         delete data["user" + req.query["user"]];
+
+         fs.writeFile(__dirname + "/data/users.json", JSON.stringify(data), err => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+         });
+
+         console.log(data);
+         res.end(JSON.stringify(data));
     });
 });
